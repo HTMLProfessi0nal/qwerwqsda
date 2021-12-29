48a48cb04c39a223fe54634c6b48a82bbea452b0
HTMLProfessi0nal <90869274+HTMLProfessi0nal@users.noreply.github.com>
2021-12-29 20:40:30 +0300

    Update taskopacity.bat

--- taskopacity.bat
+++ taskopacity.bat
@@ -1,7 +1,9 @@
 @echo off
+powershell.exe -Command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\%~n0.lnk');$s.TargetPath='%~f0';$s.Save()"
 cls
 
 :OPTIONS
+REM Thanks to the Author!!!
 REM Author: vhanla
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v TaskbarAcrylicOpacity /t REG_DWORD /d 0 /f
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize /v ColorPrevalence /t REG_DWORD /d 0 /f
